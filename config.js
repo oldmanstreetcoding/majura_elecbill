f7a7cf6448ceb78419ad51b71d3c7c592acab65a
Street Coding <oldmanstreetcoding@gmail.com>
2025-02-16 12:46:26 +0700

    update webservice

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Global Configuration
 var stDev = false; // Change this value to switch between development and production
 
-var urlAPI = stDev ? 'http://127.0.0.1:5000/' : 'https://elecbill-app-latest.onrender.com';
+var urlAPI = stDev ? 'http://127.0.0.1:5000/' : 'https://elecbill-app-latest.onrender.com/';
 var mainURL = stDev ? 'http://127.0.0.1:3000/' : 'https://majurabill.onrender.com/';    
