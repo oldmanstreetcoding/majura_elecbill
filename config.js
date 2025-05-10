736e907b323b7d2c38bcf0d873a8b3ef49e581ed
Street Coding <oldmanstreetcoding@gmail.com>
2025-05-10 21:07:12 +0700

    update backend app to distirbute daily cost faily

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Global Configuration
 var stDev = false; // Change this value to switch between development and production :D
 var urlAPI = stDev ? 'http://127.0.0.1:5000/' : 'https://elecbill-app-latest.onrender.com/';
-var mainURL = stDev ? 'http://127.0.0.1:3000/' : 'https://majurabill.onrender.com/';    
+var mainURL = stDev ? 'http://127.0.0.1:3000/' : 'https://majurabill.onrender.com/';
